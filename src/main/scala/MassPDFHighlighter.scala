17704fbdec8b2ec2e81e860e1a3cc376c09af855
Mattia Amato <mattia.amato@uzh.ch>
2015-07-13 14:51:02 +0200

    Identify highlighted text in yellow or green. Copy whole image in new png file. Still doesn't work properly

--- src/main/scala/MassPDFHighlighter.scala
+++ src/main/scala/MassPDFHighlighter.scala
@@ -28,7 +28,7 @@ object MassPDFHighlighter extends App with LazyLogging{
 
 
 		new PDFPermuter(f.getAbsolutePath).permuteForEachCombinationOf(colorToStrings).zipWithIndex.foreach(highlighter => {
-			print(highlighter._2 +" highlighting combination of " + highlighter._1.instructions)
+			print(s"${highlighter._2} highlighting combination of ${highlighter._1.instructions}")
 
 			Some(new BufferedOutputStream(new FileOutputStream(outputDir + highlighter._2 + "_" + f.getName))).foreach(s => {
 				s.write(highlighter._1.highlight())
@@ -38,9 +38,9 @@ object MassPDFHighlighter extends App with LazyLogging{
 
     logger.debug("Starting conversion PDF2PNG...")
     //Convert all the PDFs to PNG
-    new File(outputDir).listFiles().foreach(pdfFile => {
+    new File(outputDir).listFiles().par.foreach(pdfFile => {
       try {
-        ("convert -density 100 output/"+pdfFile.getName + " output/" + pdfFile.getName + ".png").!!
+        ("convert -density 150 output/"+pdfFile.getName + " output/" + pdfFile.getName + ".png").!!
         logger.debug("File: " + pdfFile.getName + ", successfully converted to PNG")
       } catch {
         case e: Exception => e.printStackTrace()
--- src/main/scala/snippet/MainSnippet.scala
+++ src/main/scala/snippet/MainSnippet.scala
@@ -1,10 +1,14 @@
 package snippet
 
-import java.io.File
+import java.awt.Color
+import java.awt.image.ColorModel
+import java.io.{FileInputStream, File}
 
-import ar.com.hjg.pngj.chunks.ChunkCopyBehaviour
-import ar.com.hjg.pngj.{ImageLineHelper, ImageLineInt, PngWriter, PngReader}
+import ar.com.hjg.pngj.chunks.{PngChunkTextVar, ChunkCopyBehaviour}
+import ar.com.hjg.pngj._
 import com.typesafe.scalalogging.LazyLogging
+
+import scala.collection.mutable
 
 /**
  * Created by mattia on 13.07.15.
@@ -16,26 +20,56 @@ object MainSnippet extends App with LazyLogging{
   new File(snippetsDir).mkdirs()
   new File(snippetsDir).listFiles().foreach(f => f.delete())
 
-  val files : List[File] = new File("output").listFiles().filter(f => f.getName.startsWith("58_") && f.getName.endsWith(".png")).toList
+  val pngFiles : List[File] = new File("output").listFiles().filter(f => f.getName.endsWith(".png")).toList
+  //.filter(f => f.getName.startsWith("58_testpdf.pdf-")).toList
 
-  files.foreach(file => {
-    val in = new PngReader(file)
-    val out = new PngWriter(new File("output/"+file.getName), in.imgInfo, true)
-    val channels = in.imgInfo.channels
+  pngFiles.foreach(pngFile => {
+    try {
+      logger.debug("File: " + pngFile.getName)
+      val in = new PngReader(pngFile)
 
-    out.copyChunksFrom(in.getChunksList(), ChunkCopyBehaviour.COPY_ALL_SAFE)
+      logger.debug(in.imgInfo.toStringDetail)
 
-    for(row <- 0 to in.imgInfo.rows-1) {
-      val l1 = in.readRow()
-      val scanline = l1.asInstanceOf[ImageLineInt].getScanline.toList
-      for(col <- 0 to in.imgInfo.cols-1) {
-        print(ImageLineHelper.getPixelRGB8(l1, col)+", ")
-        //scanline.updated(col * channels, scanline(col * channels)/2)
-        //scanline.updated(col*channels+1, ImageLineHelper.clampTo_0_255(scanline(col*channels +1) + 20))
+      val coordsYellow = new mutable.HashMap[Int, Int]()
+      val coordsGreen = new mutable.HashMap[Int, Int]()
+
+      for(row <- 0 to in.imgInfo.rows) {
+        val l1 = in.readRow()
+        //val scanline = l1.asInstanceOf[ImageLineInt].getScanline.toList
+        for(col <- 0 to in.imgInfo.cols) {
+          val rgb8 = ImageLineHelper.getPixelRGB8(l1, col)
+
+          if(getDifference(ImageLineHelper.clampTo_0_255(Color.yellow.getRGB), ImageLineHelper.clampTo_0_255(rgb8)) < 10 ||
+            getDifference(ImageLineHelper.clampTo_0_255(Color.green.getRGB), ImageLineHelper.clampTo_0_255(rgb8)) < 10 ) {
+            logger.debug("YELLOW match FOUND. file: " + pngFile.getName)
+            coordsYellow += (row -> col)
+            val out = new PngWriter(new File("snippets/"+pngFile.getName), new ImageInfo(in.imgInfo.cols, in.imgInfo.rows, in.imgInfo.bitDepth, false), true)
+            out.copyChunksFrom(in.getChunksList(), ChunkCopyBehaviour.COPY_ALL)
+            out.getMetadata().setText(PngChunkTextVar.KEY_Description, "Identify highlighted text")
+            for (roww <- 0 to in.imgInfo.rows) {
+              out.writeRow(in.readRow(roww))
+            }
+            out.end()
+            logger.debug("End image write")
+
+          } /*else if(getRange(Color.green.getRGB, rgb8) < 1500 ) {
+            logger.debug("GREEN match FOUND. file: " + pngFile.getName)
+            coordsGreen += (row -> col)
+          }*/
+        }
       }
-      out.writeRow(l1)
+
+      in.end()
+    } catch {
+      case e: Exception => {
+        logger.error("An error occurred while analyzing the png pngFile")
+        e.printStackTrace()
+      }
     }
-    in.end()
-    out.end()
   })
+
+  def getDifference(color:Int, color2: Int): Int = {
+    Math.abs(color - color2)
+  }
+
 }
