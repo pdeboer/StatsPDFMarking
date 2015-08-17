263984da37c0e3129a538ad6839afee1dcc067b1
Patrick de Boer <pdeboer@mit.edu>
2015-08-17 11:39:11 +0200

    heap space saving

--- src/main/scala/input/bmc/BMJDAL.scala
+++ src/main/scala/input/bmc/BMJDAL.scala
@@ -24,7 +24,7 @@ object BMJDAL {
 		sql"""SELECT DISTINCT p.id, t.body, p.url, left(issue,4) AS y
 		FROM bmjpdftext t INNER JOIN bmjpaper p
 		WHERE LOWER(t.body) LIKE $likeTerm"""
-			.map(r => BMJPaperBody(r.long(1))(WeakReference(r.string(2)), r.string(3), r.int(4))).list().apply()
+			.map(r => new BMJPaperBody(r.long(1), WeakReference(r.string(2)), r.string(3), r.int(4))).list().apply()
 	}
 
 	def getPaperBody(id: Long) = DB readOnly { implicit session => sql"SELECT body FROM bmjpdftext WHERE paperId = $id"
@@ -32,10 +32,24 @@ object BMJDAL {
 	}
 }
 
-case class BMJPaperBody(override val id: Long)(var _body: WeakReference[String], url: String, year: Int) extends DBPaperBody(id)("", url, year) {
-	override def body = _body.get.getOrElse({
+class BMJPaperBody(id: Long, var _body: WeakReference[String], url: String, year: Int) extends DBPaperBody(id, "", url, year) {
+	override def body: String = _body.get.getOrElse({
 		val b: String = BMJDAL.getPaperBody(id)
 		_body = WeakReference(b)
 		b
 	})
+
+	override def equals(other: Any): Boolean = other match {
+		case that: BMJPaperBody =>
+			(that canEqual this) &&
+				id == that.id
+		case _ => false
+	}
+
+	override def canEqual(other: Any): Boolean = other.isInstanceOf[BMJPaperBody]
+
+	override def hashCode(): Int = {
+		val state = Seq(id)
+		state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+	}
 }
