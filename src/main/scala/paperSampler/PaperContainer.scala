e655c1ec0ea6feda1e8d1b54c4964e1ef1232234
Mattia Amato <mattia.amato@uzh.ch>
2015-09-22 10:28:50 +0200

    get papers containing more than 0 occurrences of the method needed

--- src/main/scala/paperSampler/PaperContainer.scala
+++ src/main/scala/paperSampler/PaperContainer.scala
@@ -26,7 +26,7 @@ class PaperContainer {
   }
 
   def removeRandomPaper(method: String) : Option[Paper] = {
-    val shuffled = Random.shuffle(methodPapers.getOrElse(method, List.empty[Paper]))
+    val shuffled = Random.shuffle(methodPapers.getOrElse(method, List.empty[Paper]).filter(_.methods.get(method).get > 0))
     val toRemove = shuffled.headOption
     val rest = shuffled.drop(1)
     methodPapers += method -> rest
